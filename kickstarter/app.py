95a7380fa3f1caf2acc19ed4cba2e0dbe25d0f21
MrDoberman <44187619+MrDoberman@users.noreply.github.com>
2022-02-02 17:49:45 -0400

    Update app.py

--- kickstarter/app.py
+++ kickstarter/app.py
@@ -19,7 +19,7 @@ def create_app():
         return render_template('next.html', title='Home')
     
 
-    # User route
+   # User route
     @app.route('/user', methods=['GET', 'POST'])
     def user():
         '''User view - input page for users'''
@@ -29,19 +29,24 @@ def create_app():
             dict1=request.values
             
             print('user function running')
-            feature_list=list(dict1.values())
-            column_list=list(dict1.keys())
-            df = pd.DataFrame(columns=column_list)
-            df.loc[len(df)] = feature_list
+            
             date1= dict1.get("date_created")
             date2= dict1.get("date_launched")
             date3 = datetime.datetime.strptime(date1,"%Y-%m-%d")
             date4 = datetime.datetime.strptime(date2,"%Y-%m-%d")
             unixtime = datetime.datetime.timestamp(date4)-datetime.datetime.timestamp(date3)
+          
+            columnlist=['country_code',	'month_created', 'backers_count',	'category',	'currency_code',	
+            'project_length', 'goal', 'usd_pledged', 'staff_pick']
+
+            values=[dict1.get("country_code"),dict1.get("month_launched"),dict1.get("backers_count"),dict1.get("category"),
+            dict1.get("currency_code"),unixtime,dict1.get("goal"),dict1.get("usd_pledged"),dict1.get("staff_pick")]
+
+            df = pd.DataFrame(columns=columnlist)
+            df.loc[len(df)] = values
             
-            print(unixtime)    
-                  
-        return render_template('next.html', title='User')
+            print(df)    
+            print(dict1)    
 
     
     return app
